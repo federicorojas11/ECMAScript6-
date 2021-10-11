0c87a4bcec3cbe0e5cd41c8865718354e0a22855
Federico Rojas <federico.rojas.121@gmail.com>
2021-10-11 18:03:07 -0300

    es11 - nuevas funcionalidades

--- es11/index.js
+++ es11/index.js
@@ -1,7 +1,49 @@
-
+// Importacion de modulos
 const button = document.getElementById("btn");
 
 button.addEventListener("click", async function () {
     const module = await import("./file.js");
     module.hello();
 })
+
+// Big INT: permite trabajar con numeros mayores a 2^53
+
+// 1- Añadiendo una "n" al final activas el big int en el valor
+const aBigNumber = 9007199254740991n;
+
+// 2- Con el metodo BigInt activas el valor
+const anotherBigNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+// Promise All Settled
+const promise1 = new Promise((resolve,reject) => reject("reject"));
+const promise2 = new Promise((resolve,reject) => resolve("resolve"));
+const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+// Global This
+
+console.log(window);
+console.log(globalThis);
+
+// Nuevo operador lógico: null | undefined operator
+const foo = null ?? 'default string';
+console.log(foo);
+
+const foo2 = 'not null' ?? 'default string';
+console.log(foo2)
+
+// Optional chaining (identifica si el valor es undefined sin romper la app)
+const user = {};
+console.log(user?.profile?.email);
+
+if(user?.profile?.email) {
+    console.log('email')
+} else{
+    console.log('fail')
+}
